0e7c5921734fdf7b79196af2feb7b940e58ad7fe
Nicolas <nicolasfardim@gmail.com>
2026-07-09 15:52:19 +0100

    flake8 fix

--- pymodule_00/ex7/ft_seed_inventory.py
+++ pymodule_00/ex7/ft_seed_inventory.py
@@ -1,10 +1,13 @@
 def ft_seed_inventory(seed_type: str, quantity: int, unit: str) -> None:
     match unit.lower():
         case 'packets':
-            print(f'{seed_type.capitalize()} seeds: {quantity} packets available')
+            print(f'{seed_type.capitalize()} seeds: '
+                  f'{quantity} packets available')
         case 'grams':
-            print(f'{seed_type.capitalize()} seeds: {quantity} grams total')
+            print(f'{seed_type.capitalize()} seeds: '
+                  f'{quantity} grams total')
         case 'area':
-            print(f'{seed_type.capitalize()} seeds: covers {quantity} square meters')
+            print(f'{seed_type.capitalize()} seeds: covers '
+                  f'{quantity} square meters')
         case _:
             print('Unknown unit type')
