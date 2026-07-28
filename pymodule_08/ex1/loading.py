8ecda1a54983406a0ad937dfd03c8ec2f72ffa59
Nicolas <nicolasfardim@gmail.com>
2026-07-28 16:49:22 +0100

    added ex1

--- pymodule_08/ex1/loading.py
+++ pymodule_08/ex1/loading.py
@@ -1,5 +1,9 @@
 import importlib
 
+import numpy as np
+import pandas as pd
+import requests
+
 
 def check_dependencies():
     pkgs = {
@@ -18,9 +22,42 @@ def check_dependencies():
         print(e)
 
 
+def request_data() -> requests.Response:
+    url = "https://dummyjson.com/users"
+    response: requests.Response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+    else:
+        print("Failed to fetch data")
+        exit()
+    return data
+
+
+def extract_data(data: requests.Response) -> tuple[pd.DataFrame, pd.DataFrame]:
+    users = data["users"]
+    df = pd.DataFrame(users)
+
+    tall_people = df[df["height"] > 180][
+        ["firstName", "lastName", "height"]
+    ]
+    small_people = df[df["height"] < 170][
+        ["firstName", "lastName", "height"]
+    ]
+    return small_people, tall_people
+
+
+def analyze(small_people: pd.DataFrame, tall_people: pd.DataFrame) -> tuple[np.float64, np.float64]:
+    average_small = np.mean(small_people["height"])
+    average_tall = np.mean(tall_people["height"])
+    return average_small, average_tall
+
+
 def main():
     print("LOADING STATUS: Loading programs...")
-    check_dependencies()
+    data: requests.Response = request_data()
+    small, tall = extract_data(data)
+    analyze(small, tall)
 
 
 if __name__ == '__main__':
